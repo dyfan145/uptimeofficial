382b7a79d724d1520ad74c15e31d620415d3d285
dyfan145 <69150154+dyfan145@users.noreply.github.com>
2021-02-20 10:19:09 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const axios = require("axios")
-const urls = ["https://foamy-plausible-fridge.glitch.me"]
+const urls = ["https://polydactyl-nine-single.glitch.me"]
 setInterval(function() {
             urls.forEach(url => {
             axios.get(url).then(console.log("Pong at " + Date.now())).catch(() => {});
